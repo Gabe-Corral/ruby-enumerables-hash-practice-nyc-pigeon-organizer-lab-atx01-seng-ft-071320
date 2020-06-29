5e7ac2fb166f5d34438323078f47ba36c0b7b086
Joseph Corral <78gabe.corral@gmail.com>
2020-06-29 23:32:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_data = []
+  data.each do |color, name|
+    pigeon_data.push(data[color])
+  end
+  pigeon_list = {"theo" => {color: ["purple", "grey"], gender: ["male"], lives: ["Subway"]}
+    
+  }
+  puts pigeon_data
 end
