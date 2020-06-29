6ab44d77a5d31711a20b9ce663fa5ab9b99bd0a5
Joseph Corral <78gabe.corral@gmail.com>
2020-06-29 23:51:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
+      #loops to the next level to access names
       names.each do |name|
+        
         if !final_array[name]
           final_array[name] = {}
         end
