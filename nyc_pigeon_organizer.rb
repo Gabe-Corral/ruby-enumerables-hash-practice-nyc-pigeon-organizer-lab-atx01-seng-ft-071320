e46536056f097e2237f48770beb8e622b62d1811
Joseph Corral <78gabe.corral@gmail.com>
2020-06-29 23:39:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   final_hash = data.each_with_object({}) do |key, value, finish_array|
     value.each do |inner_key, names|
+      name.each do |name|
         if !finish_array[name]
           finish_array[name] = {}
         end
